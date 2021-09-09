ada6b62776bea0d029a662c9ef42ef209900f2de
Vincent-kipngeno <kipngenovi2001@gmail.com>
2021-09-09 12:20:56 +0300

    Debugging

--- js/binaryCalculator.js
+++ js/binaryCalculator.js
@@ -56,35 +56,43 @@ function clickEql(){
 
 
 function getResult(){
-    arithmetic_regex = /[^\d()]+|[\d.]+/g;
-    splitUp = res.innerText.match(arithmetic_regex);
-    operator_regex = /[\+\-\*\/]+/g;
+    let arithmetic_regex = /[^\d()]+|[\d.]+/g;
+    let operator_regex = /[\+\-\*\/]+/g;
+    //split the expression into array of both operators and binary numbers
+    let splitUp = res.innerText.match(arithmetic_regex);
+    //get array of operators only
+    var operators = res.innerText.match(operator_regex);
 
-
-    while(operator_regex.test(splitUp)){
+    while(operators.length != 0){
         if(splitUp.includes('/')){
             divIndex = splitUp.indexOf('/');
             divResult = Math.floor(parseInt(splitUp[divIndex-1], 2) / parseInt(splitUp[divIndex+1], 2));
             splitUp.splice(divIndex-1, 3, divResult.toString(2));
+            operators.splice(operators.indexOf('/'), 1);
         }
 
         else if(splitUp.includes('*')){
             mulIndex = splitUp.indexOf('*');
             mulResult = parseInt(splitUp[mulIndex-1], 2) * parseInt(splitUp[mulIndex+1], 2);
             splitUp.splice(mulIndex-1, 3, mulResult.toString(2));
+            operators.splice(operators.indexOf('*'), 1);
         }
 
         else if(splitUp.includes('+')){
             sumIndex = splitUp.indexOf('+');
             sumResult = parseInt(splitUp[sumIndex-1], 2) + parseInt(splitUp[sumIndex+1], 2);
-            console.log(sumResult.toString(2));
+            console.log(sumResult.toString());
             splitUp.splice(sumIndex-1, 3, sumResult.toString(2));
+            operators.splice(operators.indexOf('+'), 1);
         }
 
         else{
-            subIndex = splitUp.indexOf('-');
-            subResult = parseInt(splitUp[subIndex-1], 2) - parseInt(splitUp[subIndex+1], 2);
-            splitUp.splice(subIndex-1, 3, subResult.toString(2));
+            if(splitUp.includes('-')){
+                subIndex = splitUp.indexOf('-');
+                subResult = parseInt(splitUp[subIndex-1], 2) - parseInt(splitUp[subIndex+1], 2);
+                splitUp.splice(subIndex-1, 3, subResult.toString(2));
+                operators.splice(operators.indexOf('-'), 1);
+            }
         }
     }
     return  splitUp.toString();
